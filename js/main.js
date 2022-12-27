33d29a392f1c937a8d3042e31af5ea6a58940499
Yash Bhardwaj <imyashbhardwaj@gmail.com>
2022-12-27 15:55:31 +0530

    fix: typed text animation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 (function ($) {
     "use strict";
     // Typed Initiate
-    if ($('.hero .herotext h2').length == 1) {
-        var typed_strings = $('.hero .herotext .typed-text').text();
-        var typed = new Typed('.hero .herotext h2', {
-            strings: typed_strings.split(', '),
+    if ($('.hero h2').length == 1) {
+        var typed = new Typed('.hero h2', {
+            strings: ["Web designer", "Content Writer", "Chemist"],
             typeSpeed: 100,
             backSpeed: 20,
             smartBackspace: false,
