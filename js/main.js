fbd0faa253b1ec2a5d0676a63995f1f34eaa8c70
imprarthana <prarthanachaudhary07@gmail.com>
2022-12-29 21:05:22 +0530

    Feat: Responsiveness to nav bar
    Nav bar was disappearing in mobile view and small screens

--- js/main.js
+++ js/main.js
@@ -11,3 +11,15 @@
         });
     }
 })(jQuery);
+
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
