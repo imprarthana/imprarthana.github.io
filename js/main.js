ae1f6a02c990ec39eaa7147712ca0b066f087b82
imprarthana <prarthanachaudhary07@gmail.com>
2023-01-10 14:49:25 +0530

    Feat: Added responsiveness to about hero section

--- js/main.js
+++ js/main.js
@@ -12,6 +12,20 @@
     }
 })(jQuery);
 
+(function ($) {
+  "use strict";
+  // Typed Initiate
+  if ($('.hero-content-resp .herotext h2').length == 1) {
+      var typed = new Typed('.hero-content-resp .herotext h2', {
+          strings: ["Web designer", "Content Writer", "Chemist"],
+          typeSpeed: 100,
+          backSpeed: 20,
+          smartBackspace: false,
+          loop: true
+      });
+  }
+})(jQuery);
+
 
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
